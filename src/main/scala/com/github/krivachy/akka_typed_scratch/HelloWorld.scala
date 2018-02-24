c92a6bb22e2f5e9a6533b4c479412a712d4bb545
Akos Krivachy <akos.krivachy@gmail.com>
2018-02-24 16:45:45 +0000

    WIP

--- src/main/scala/com/github/krivachy/akka_typed_scratch/HelloWorld.scala
+++ src/main/scala/com/github/krivachy/akka_typed_scratch/HelloWorld.scala
@@ -3,6 +3,8 @@ package com.github.krivachy.akka_typed_scratch
 import akka.typed._
 import akka.typed.scaladsl.Actor
 
+import scala.annotation.tailrec
+
 
 object HelloWorld {
   final case class Greet(whom: String, replyTo: ActorRef[Greeted])
@@ -13,5 +15,36 @@ object HelloWorld {
     msg.replyTo ! Greeted(msg.whom)
     Actor.same
   }
-}
 
+  sealed trait CounterCommand {
+    def replyTo: ActorRef[CounterResponse]
+  }
+  final case class Inc(replyTo: ActorRef[CounterResponse]) extends CounterCommand
+  final case class Dec(replyTo: ActorRef[CounterResponse]) extends CounterCommand
+  final case class Set(value: Int, replyTo: ActorRef[CounterResponse]) extends CounterCommand
+  final case class Query(replyTo: ActorRef[CounterResponse]) extends CounterCommand
+
+  sealed trait CounterResponse
+  final case class Success(count: Int) extends CounterResponse
+  final case class Failure(exception: Exception) extends CounterResponse
+
+  val counter = counterBehaviour(0)
+//  @tailrec
+  def counterBehaviour(currentCount: Int): Behavior[CounterCommand] = Actor.immutable[CounterCommand] {
+    (ctx, command) =>
+//      ctx.()
+      val updatedCount = command match {
+        case _: Inc => currentCount + 1
+        case _: Dec => currentCount - 1
+        case Set(value, _) => value
+        case _: Query => currentCount
+      }
+
+      command.replyTo ! Success(updatedCount)
+      counterBehaviour(updatedCount)
+  }
+//  onSignal {
+//    case () =>
+//  }
+
+}
